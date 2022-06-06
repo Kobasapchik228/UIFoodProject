0d6f7de3513818e84cd31787df9dae7e8be7f4f8
Kobasapchik228 <Kobasap2283228@gmail.com>
2022-06-06 12:02:43 +0300

    modal winwod creation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
+    //Tabs
     const tabs = document.querySelectorAll(".tabheader__item"),
         tabsContent = document.querySelectorAll(".tabcontent"),
         tabsParent = document.querySelector(".tabheader__items");
@@ -34,5 +35,41 @@ window.addEventListener("DOMContentLoaded", () => {
             })
         }
     })
+
+    //Modal window
+    const modalOpen = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector(".modal")
+
+    modalOpen.forEach(item => {
+        item.addEventListener("click", () => {
+            // modal.classList.add('show')
+            // modal.classList.remove('hide')
+            modal.style.display = "block"
+            document.body.style.overflow = "hidden"
+        })
+    })
+    function closeModal() {
+        modal.style.display = "none"
+        document.body.style.overflow = ""
+    }
+
+    document.querySelector('[data-close]').addEventListener("click", () => {
+        // modal.classList.add('hide')
+        //modal.classList.remove('show')
+        closeModal()
+    })
+
+    modal.addEventListener("click", (e) => {
+        if (e.target === modal) {
+            closeModal()
+        }
+    })
+
+    document.addEventListener("keydown", (e) => {
+        if (e.code === "Escape" /**&& modal.classList.contains('show')*/) {
+            closeModal()
+        }
+    })
+
 })
 
