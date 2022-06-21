a215ae4a39c4b5e340da69cef8a98543e7885207
Kobasapchik228 <Kobasap2283228@gmail.com>
2022-06-21 11:20:37 +0300

    added nav for slides

--- js/script.js
+++ js/script.js
@@ -292,6 +292,7 @@ window.addEventListener("DOMContentLoaded", () => {
         .then(data => data.json())
         .then(res => console.log(res))
 
+    //-----------------------------Sliders and nav for slides-----------------
     //----------------------------Slider  first version
 
     // const slides = document.querySelectorAll('.offer__slide'),
@@ -343,9 +344,10 @@ window.addEventListener("DOMContentLoaded", () => {
     // })
 
 
-    //-----------------------------------------------Slider second version
-
+    //-----------------------------------------------Slider second version---------------------------------
+    //---------------------------with nav for slides-------------------------------------------------------
     const slides = document.querySelectorAll('.offer__slide'),
+        slider = document.querySelector('.offer__slider'),
         prev = document.querySelector('.offer__slider-prev'),
         next = document.querySelector('.offer__slider-next'),
         total = document.querySelector('#total'),
@@ -372,6 +374,65 @@ window.addEventListener("DOMContentLoaded", () => {
     slides.forEach(slide => {
         slide.style.width = width
     })
+      
+    //added nav 
+    slider.style.position = `relative`
+
+    const indicators = document.createElement('ol'),
+        dots = []
+    indicators.classList.add('carousel-indicators')
+    indicators.style.cssText = `
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 15;
+    display: flex;
+    justify-content: center;
+    margin-right: 15%;
+    margin-left: 15%;
+    list-style: none;
+    `;
+    slider.append(indicators);
+
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li')
+        dot.setAttribute(`data-slide-to`, i + 1)
+        dot.style.cssText = `
+        box-sizing: content-box;
+    flex: 0 1 auto;
+    width: 30px;
+    height: 6px;
+    margin-right: 3px;
+    margin-left: 3px;
+    cursor: pointer;
+    background-color: #fff;
+    background-clip: padding-box;
+    border-top: 10px solid transparent;
+    border-bottom: 10px solid transparent;
+    opacity: .5;
+    transition: opacity .6s ease;
+        `
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+        indicators.append(dot)
+        dots.push(dot)
+    }
+
+    function appointCurrentValue() {
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`
+        } else {
+            current.textContent = slideIndex
+        }
+    }
+
+    function appointDotsOppacity() {
+        dots.forEach(dot => dot.style.opacity = `.5`);
+        dots[slideIndex - 1].style.opacity = 1;
+    }
+   
 
     next.addEventListener('click', () => {
         if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
@@ -387,11 +448,9 @@ window.addEventListener("DOMContentLoaded", () => {
             slideIndex++
         }
 
-        if (slides.length < 10) {
-            current.textContent = `0${slideIndex}`
-        } else {
-            current.textContent = slideIndex
-        }
+        appointCurrentValue()
+        appointDotsOppacity()
+
     })
 
     prev.addEventListener('click', () => {
@@ -403,18 +462,27 @@ window.addEventListener("DOMContentLoaded", () => {
 
         slidesField.style.transform = `translateX(-${offset}px)`
 
-
         if (slideIndex == 1) {
             slideIndex = slides.length
         } else {
             slideIndex--
         }
 
-        if (slides.length < 10) {
-            current.textContent = `0${slideIndex}`
-        } else {
-            current.textContent = slideIndex
-        }
+        appointCurrentValue()
+        appointDotsOppacity()
+    })
+
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute(`data-slide-to`)
+
+            slideIndex = slideTo
+            offset = +width.slice(0, width.length - 2) * (slideTo - 1)
+            slidesField.style.transform = `translateX(-${offset}px)`
+
+            appointCurrentValue()
+            appointDotsOppacity()
+        })
     })
 
 
