d4e713d8b2a694dc0888cd88cd5ac5238422152d
Kobasapchik228 <Kobasap2283228@gmail.com>
2022-06-30 19:21:12 +0300

    Added modules and added likely finally commit

---
+++ js/modules/tabs.js
@@ -0,0 +1,39 @@
+function tabs(tabsSelector, tabsContentSelector, tabsParentSelector, activeClass) {
+    const tabs = document.querySelectorAll(tabsSelector),
+        tabsContent = document.querySelectorAll(tabsContentSelector),
+        tabsParent = document.querySelector(tabsParentSelector);
+
+    function hideAllTabs() {
+        tabsContent.forEach((item) => {
+            item.style.display = "none"
+        })
+
+        tabs.forEach((item) => {
+            item.classList.remove(activeClass)
+        })
+
+
+    }
+
+    function showTab(i = 0) {
+        tabsContent[i].style.display = "block"
+        tabs[i].classList.add(activeClass)
+    }
+
+    hideAllTabs()
+    showTab()
+
+    tabsParent.addEventListener("click", (e) => {
+        let target = e.target
+        if (target && target.classList.contains(tabsSelector.slice(1))) {
+            tabs.forEach((item, i) => {
+                if (item == target) {
+                    hideAllTabs()
+                    showTab(i)
+                }
+            })
+        }
+    })
+}
+
+export default tabs
